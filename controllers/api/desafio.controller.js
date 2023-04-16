ff980451b013786df8d0e1d46c57a5fb7163e3a0
fabidota <62340981+fabidota@users.noreply.github.com>
2023-04-16 10:52:42 -0300

    Initial commit

---
+++ controllers/api/desafio.controller.js
@@ -0,0 +1,76 @@
+﻿var express = require('express');
+var router = express.Router();
+//var pessoaService = require('services/pessoa.service');
+
+// routes
+
+//router.post('/calculaIterativo/:m/:n', calculaIterativo);
+//router.post('/calculaRecursivo/:m/:n', calculaRecursivo);
+router.post('/:metodo/:m/:n', calculaProdutorio);
+
+module.exports = router;
+
+function calculaProdutorio(req, res) {
+    var metodo = req.params.metodo;
+    let m = parseInt(req.params.m);
+    let n = parseInt(req.params.n);
+
+    let multiplicacao = 1.0;
+    switch (metodo) {
+        case 'iterativo':
+            multiplicacao = produtorioIterativo(m, n);
+            break;
+        case 'recursivo':
+            multiplicacao = produtorioRecursivo(m, n);
+            break;
+    }
+    res.send(JSON.stringify({ metodo, multiplicacao }));
+}
+
+
+
+
+
+
+
+
+
+
+function produtorioRecursivo(m, n) {
+    if (m == n)
+        return m + (1.0 / m);
+    else
+        return (m + (1.0 / m)) * produtorioRecursivo(m + 1, n);
+}
+
+function produtorioIterativo(m, n) {
+    let multiplicacao = 1.0;
+
+    for (let i = m; i <= n; i++)
+        multiplicacao = multiplicacao * (i + (1.0 / i));
+
+    return multiplicacao;
+}
+
+/*
+function calculaIterativo(req, res) {
+    let m = parseInt(req.params.m);
+    let n = parseInt(req.params.n);
+
+    let multiplicacao = 1.0;
+
+    for (let i = m; i <= n; i++)
+        multiplicacao = multiplicacao * (i + (1.0 / i));
+    res.send(JSON.stringify({ multiplicacao }));
+
+}
+
+function calculaRecursivo(req, res) {
+    let m = parseInt(req.params.m);
+    let n = parseInt(req.params.n);
+
+    var multiplicacao = produtorioRecursivo(m, n);
+
+    res.send(JSON.stringify({ multiplicacao }));
+
+}*/
